04112aa6d0cb9b5b0faf2cb8a8944ddf4d2d90a6
gonzalo.lozg <gonzalo.lozg@gmail.com>
2022-08-28 23:14:24 -0500

    block dark theme

--- app/src/main/java/com/ulearning/ulearning_app/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/ulearning/ulearning_app/data/remote/api/AuthApi.kt
@@ -1,10 +1,12 @@
 package com.ulearning.ulearning_app.data.remote.api
 
+import com.ulearning.ulearning_app.data.remote.entities.BaseResponse
 import com.ulearning.ulearning_app.data.remote.entities.request.LoginRequest
 import com.ulearning.ulearning_app.data.remote.entities.response.LoginResponse
+import com.ulearning.ulearning_app.data.remote.entities.response.SubscriptionResponse
+import com.ulearning.ulearning_app.data.remote.utils.SettingRemote
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 interface AuthApi {
@@ -13,5 +15,10 @@ interface AuthApi {
     suspend fun login(
         @Body request: LoginRequest
     ): Response<LoginResponse>
+
+    @GET("profile")
+    suspend fun subscription(
+        @Header(SettingRemote.AUTHORIZATION) token: String,
+    ): Response<BaseResponse<List<SubscriptionResponse>>>
     
 }
---
+++ app/src/main/java/com/ulearning/ulearning_app/data/remote/entities/response/ProfileReponse.kt
@@ -0,0 +1,47 @@
+package com.ulearning.ulearning_app.data.remote.entities.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProfileReponse(
+    @SerializedName("address")
+    val address: Any? = Any(),
+    @SerializedName("avatar")
+    val avatar: Any? = Any(),
+    @SerializedName("country")
+    val country: Any? = Any(),
+    @SerializedName("country_id")
+    val countryId: Any? = Any(),
+    @SerializedName("date_of_birth")
+    val dateOfBirth: Any? = Any(),
+    @SerializedName("document_number")
+    val documentNumber: Any? = Any(),
+    @SerializedName("document_type")
+    val documentType: String? = "",
+    @SerializedName("email")
+    val email: String? = "",
+    @SerializedName("first_name")
+    val firstName: String? = "",
+    @SerializedName("gender")
+    val gender: String? = "",
+    @SerializedName("id")
+    val id: Int? = 0,
+    @SerializedName("last_name")
+    val lastName: String? = "",
+    @SerializedName("name")
+    val name: String? = "",
+    @SerializedName("permissions")
+    val permissions: List<String?>? = listOf(),
+    @SerializedName("phone")
+    val phone: Any? = Any(),
+    @SerializedName("phone_code")
+    val phoneCode: Any? = Any(),
+    @SerializedName("plan")
+    val plan: Any? = Any(),
+    @SerializedName("role")
+    val role: String? = "",
+    @SerializedName("second_last_name")
+    val secondLastName: Any? = Any(),
+    @SerializedName("suspended_at")
+    val suspendedAt: Any? = Any()
+)
