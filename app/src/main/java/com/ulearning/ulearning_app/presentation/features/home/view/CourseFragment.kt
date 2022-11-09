155a886f15b1e8157cf03f092fb2fe5c285f9cd3
gonzalo.lozg <gonzalo.lozg@gmail.com>
2022-11-08 23:39:22 -0500

    WP: added viewPager in home

---
+++ app/src/main/java/com/ulearning/ulearning_app/presentation/features/home/view/CourseFragment.kt
@@ -0,0 +1,28 @@
+package com.ulearning.ulearning_app.presentation.features.home.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ulearning.ulearning_app.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CourseFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course, container, false)
+    }
+
+}
